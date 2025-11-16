8e785ae2db7459c85b1b93b407116841edd301b1
Danil K. <danil.kukuev@gmail.com>
2025-11-16 17:11:11 +0300

    fix docstring

--- src/DaliMQTT/dali/DaliTypes.hxx
+++ src/DaliMQTT/dali/DaliTypes.hxx
@@ -19,12 +19,14 @@ namespace daliMQTT
 
     /**
      * @brief Конвертирует DaliLongAddress_t в его строковое (HEX) представление.
-     * @param addr 24-битный long address.
-     * @return std::array<char, 7> с null-терминированной строкой (e.g., "A1B2C3").
+     * @param addr 24-bit DALI long address.
+     * @return std::array<char, 7> with HEX string of long addr.
      */
     inline LongAddrStr longAddressToString(const DaliLongAddress_t addr) {
         LongAddrStr result{};
-        if (auto [ptr, ec] = std::to_chars(result.data(), result.data() + 6, addr & 0xFFFFFF, 16); ec == std::errc()) {
+        if (auto [ptr, ec] =
+                std::to_chars(result.data(), result.data() + 6, addr & 0xFFFFFF, 16);
+                ec == std::errc()) {
             if (const size_t len = ptr - result.data(); len < 6) {
                 std::move_backward(result.data(), result.data() + len, result.data() + 6);
                 std::fill_n(result.data(), (6 - len), '0');
@@ -35,14 +37,14 @@ namespace daliMQTT
                 }
             }
         }
-        result[6] = '\0'; // Null-terminator
+        result[6] = '\0';
         return result;
     }
 
     /**
      * @brief Конвертирует строковое HEX представление в DaliLongAddress.
-     * @param s Строковое представление (e.g., "A1B2C3").
-     * @return Optional DaliLongAddress.
+     * @params string DaliLongAddress.
+     * @return Optional DaliLongAddress_t or nullopt.
      */
     inline std::optional<DaliLongAddress_t> stringToLongAddress(std::string_view s) {
         DaliLongAddress_t addr = 0;
