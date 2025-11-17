535936cb72b67efb7566c88e7d9cba70cde6502a
Danil K. <danil.kukuev@gmail.com>
2025-11-17 15:29:28 +0300

    fix build: default BUILD_TYPE to Debug

--- scripts/configure_esp_platform.cmake
+++ scripts/configure_esp_platform.cmake
@@ -4,8 +4,15 @@ if(NOT DEFINED CMAKE_TOOLCHAIN_FILE)
             "e.g., run 'get_idf' or '. $HOME/esp/esp-idf/export.sh'")
 endif ()
 
-message(NOTICE "Using C compiler: " ${CMAKE_C_COMPILER})
+message(NOTICE "Using C/C++ compiler: " ${CMAKE_CXX_COMPILER})
+message(NOTICE "Using Build Tool: " ${CMAKE_BUILD_TOOL})
 message(NOTICE "IDF framework at: " $ENV{IDF_PATH})
+
+set(SUPPORTED_BUILD_TYPES "Release" "Debug")
+
+if(NOT CMAKE_BUILD_TYPE IN_LIST SUPPORTED_BUILD_TYPES)
+    message(WARNING "You are using an unsupported build type: ${CMAKE_BUILD_TYPE}")
+endif()
 
 set(ESP_BUILD_UTILS_PATH ${CMAKE_CURRENT_SOURCE_DIR})
 
@@ -19,13 +26,7 @@ include(../scripts/build_firmware.cmake)
 include(../scripts/size_components.cmake)
 include(../scripts/make_webui.cmake)
 
-set(SUPPORTED_BUILD_TYPES "Release" "Debug")
-
-if(NOT CMAKE_BUILD_TYPE IN_LIST SUPPORTED_BUILD_TYPES)
-    message(WARNING "You are using an unsupported build type: ${CMAKE_BUILD_TYPE}")
-endif()
-
 include(../scripts/build_tests.cmake)
 
-message(STATUS "--------------------------------------------")
+message(NOTICE "--------------------------------------------")
 message(STATUS "Configuration done. Run ninja/make daliMQTT and ninja/make flash")
