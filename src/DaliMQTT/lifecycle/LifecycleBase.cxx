a5d1443e0125aec07a3dcbe42328e83803de8b60
danil <danil.kukuev@gmail.com>
2025-11-25 14:36:43 +0300

    add set ota done in lifecycle

--- src/DaliMQTT/lifecycle/LifecycleBase.cxx
+++ src/DaliMQTT/lifecycle/LifecycleBase.cxx
@@ -10,7 +10,7 @@
 #include "WebUI.hxx"
 #include "Wifi.hxx"
 #include "SyslogConfig.hxx"
-
+#include <esp_ota_ops.h>
 
 namespace daliMQTT
 {
@@ -25,6 +25,14 @@ namespace daliMQTT
 
         auto& web = WebUI::getInstance();
         web.start();
+        const esp_partition_t *running = esp_ota_get_running_partition();
+        esp_ota_img_states_t ota_state;
+        if (esp_ota_get_state_partition(running, &ota_state) == ESP_OK) {
+            if (ota_state == ESP_OTA_IMG_PENDING_VERIFY) {
+                ESP_LOGI(TAG, "OTA Update detected. Marking firmware as valid.");
+                esp_ota_mark_app_valid_cancel_rollback();
+            }
+        }
         ESP_LOGI(TAG, "Web server for provisioning is running.");
     }
 
@@ -67,7 +75,14 @@ namespace daliMQTT
 
         auto& web = WebUI::getInstance();
         web.start();
-
+        const esp_partition_t *running = esp_ota_get_running_partition();
+        esp_ota_img_states_t ota_state;
+        if (esp_ota_get_state_partition(running, &ota_state) == ESP_OK) {
+            if (ota_state == ESP_OTA_IMG_PENDING_VERIFY) {
+                ESP_LOGI(TAG, "OTA Update detected. Marking firmware as valid.");
+                esp_ota_mark_app_valid_cancel_rollback();
+            }
+        }
         wifi.connectToAP(config.wifi_ssid, config.wifi_password);
     }
 
