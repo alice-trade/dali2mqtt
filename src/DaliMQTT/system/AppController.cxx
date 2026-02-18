71ab65f8338f47062efd907036c687158a3544dc
Danil K. <danil.kukuev@gmail.com>
2026-02-18 14:22:47 +0300

    fixed app init
    (cherry picked from commit f677e9549952ea99d7968aba7ef5dab10f085f59)

--- src/DaliMQTT/system/AppController.cxx
+++ src/DaliMQTT/system/AppController.cxx
@@ -23,6 +23,7 @@ namespace daliMQTT
     void AppController::startProvisioningMode() {
         ESP_LOGI(TAG, "Starting in provisioning mode...");
         SystemHardwareControls::checkOtaValidation();
+
         auto& wifi = Wifi::Instance();
         wifi.init();
 
@@ -37,10 +38,12 @@ namespace daliMQTT
 
         SystemHardwareControls::checkOtaValidation();
         SystemHardwareControls::startResetConfigurationButtonMonitor();
+
+        initNetworkSubsystem();
         initDaliSubsystem();
+
         MQTTCommandProcess::Instance().init();
         WebUI::Instance().start();
-        initNetworkSubsystem();
     }
 
     void AppController::initDaliSubsystem() {
